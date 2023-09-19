87dfb103b9f9d561cf4ac937cf66b5875b686802
Mister1SI <wazzup2690@gmail.com>
2023-09-18 21:00:48 -0500

    Implement basic tests for the search function

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
         println!("Error parsing arguments: {err}");
         process::exit(1);
     });
-    println!("Searching in file \"{}\" for \"{}\"",  config.file_path, config.query);
+    //println!("Searching in file \"{}\" for \"{}\"",  config.file_path, config.query);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
