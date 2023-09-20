f1a04bf2d0dd469e45a2949e15b84feb4e16852f
Mister1SI <74750011+Mister1SI@users.noreply.github.com>
2023-09-20 14:09:54 +0000

    Optimize search() function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::env;
-use std::process;   
+use std::process;
 
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Error parsing arguments: {err}");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     //println!("Searching in file \"{}\" for \"{}\"",  config.file_path, config.query);
